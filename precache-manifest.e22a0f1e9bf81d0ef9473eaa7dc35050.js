389b6626e2adfdb5430d1a6a1e8472fe15f3c6dd
brunojustino <brunojustino@yahoo.com.br>
2022-03-31 16:07:17 -0300

    Updates

---
+++ precache-manifest.e22a0f1e9bf81d0ef9473eaa7dc35050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e014a4c3b906da21354fefb1c311a3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e7b90ba557b12267ab42",
+    "url": "/portfolio/static/css/2.9c389199.chunk.css"
+  },
+  {
+    "revision": "252163cfef18f8fb81e9",
+    "url": "/portfolio/static/css/main.8653b40b.chunk.css"
+  },
+  {
+    "revision": "e7b90ba557b12267ab42",
+    "url": "/portfolio/static/js/2.30e61608.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/portfolio/static/js/2.30e61608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252163cfef18f8fb81e9",
+    "url": "/portfolio/static/js/main.2b3feb4f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
